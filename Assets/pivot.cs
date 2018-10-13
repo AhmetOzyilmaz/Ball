7b1dfc4773890cf04d345dfdad84f720a4e577f2
AHMET ÖZYILMAZ <aozyilmaz35@gmail.com>
2018-10-13 23:23:34 +0300

    Summer work
    some designs

---
+++ Assets/pivot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pivot : MonoBehaviour {
+    public float gizmoSize = .75f;
+    public Color gizmoColor = Color.yellow;
+    
+    void OnDrowGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        Gizmos.DrawWireSphere(transform.position, gizmoSize);
+    }
+
+}
